cf8a94c6921fde7c03601ec03f7ba9e934e55f4c
mwidiger <matt.widiger@gmail.com>
2023-02-08 09:56:25 -0500

    Class for sending twilio messages (skeleton)

---
+++ app/models/twilio_message.rb
@@ -0,0 +1,21 @@
+class TwilioMessage
+  attr_reader :account_sid, :auth_token, :error_message
+  attr_accessor :from_number, :to_number, :message_text
+
+  def initialize(params = {})
+    @account_sid = params[:account_sid]
+    @auth_token = params[:auth_token]
+    @from_number = params[:from_number]
+    @to_number = params[:to_number]
+    @message_text = params[:message_text]
+    @error_message = ""
+
+    if @account_sid.blank? || @auth_token.blank?
+      @error_message = "Missing Twilio Credentials"
+    end
+  end
+
+  def send
+    return self if @error_message.present?
+  end
+end
