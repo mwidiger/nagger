f22abd49d317b4fa3f58cecfe70b0a35c5224f3b
mwidiger <matt.widiger@gmail.com>
2023-02-09 14:22:50 -0500

    environment variable management, added it to twilio messaging class

--- app/models/twilio_message.rb
+++ app/models/twilio_message.rb
@@ -1,11 +1,11 @@
 class TwilioMessage
   attr_reader :account_sid, :auth_token, :error_message
-  attr_accessor :from_number, :to_number, :message_text
+  attr_accessor :to_number, :message_text
 
   def initialize(params = {})
     @account_sid = params[:account_sid]
     @auth_token = params[:auth_token]
-    @from_number = params[:from_number]
+    @from_number = ENV['TWILIO_PHONE_NO']
     @to_number = params[:to_number]
     @message_text = params[:message_text]
     @error_message = ""
